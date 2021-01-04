682a5dabcc1595f4a171277558d582444475e486
Lairin-pdj <70496139+Lairin-pdj@users.noreply.github.com>
2021-01-04 14:57:46 +0900

    Add files via upload

---
+++ maketext.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int num = atoi(argv[1]);
+
+	FILE *fp = fopen("Text.txt", "w");
+
+	for(i = 1; i < num + 1; i++){
+		fprintf(fp, "%08d\n", i);
+	}
+}
